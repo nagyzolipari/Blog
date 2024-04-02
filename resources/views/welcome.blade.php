2a038ecc07b044c573000e7670d830c7857a3418
Dalma-sudo <nagy.dalma94@gmail.com>
2024-04-02 21:11:27 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,7 +94,6 @@
 
                                             <p class="mt-4 text-sm/relaxed">
                                                 LARAVEL LARAVEL !!!
-                                                Lara
                                                 Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                             </p>
                                         </div>
